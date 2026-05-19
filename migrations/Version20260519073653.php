502846ded14b96e493d37881c229af9f934ed529
LucasBertaud <l.bertaud@hotmail.com>
2026-05-19 09:37:50 +0200

    fix(migration): problem with doctrine migration migrate on Unknown column 'van_id'

--- migrations/Version20260519073653.php
+++ migrations/Version20260519073653.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260518130447 extends AbstractMigration
+final class Version20260519073653 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,8 @@ final class Version20260518130447 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE quote_responses CHANGE van_id id_van INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE quote_response_values ADD id INT AUTO_INCREMENT NOT NULL, CHANGE value value LONGTEXT DEFAULT NULL, DROP PRIMARY KEY, ADD PRIMARY KEY (id)');
+        $this->addSql('ALTER TABLE quote_responses ADD id_van INT DEFAULT NULL');
         $this->addSql('ALTER TABLE quote_responses ADD CONSTRAINT FK_B6BD06EEE7753313 FOREIGN KEY (id_van) REFERENCES vans (id_van)');
         $this->addSql('CREATE INDEX IDX_B6BD06EEE7753313 ON quote_responses (id_van)');
     }
@@ -28,8 +29,10 @@ final class Version20260518130447 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE quote_response_values MODIFY id INT NOT NULL');
+        $this->addSql('ALTER TABLE quote_response_values DROP id, CHANGE value value LONGTEXT NOT NULL, DROP PRIMARY KEY, ADD PRIMARY KEY (id_field, id_quote_response)');
         $this->addSql('ALTER TABLE quote_responses DROP FOREIGN KEY FK_B6BD06EEE7753313');
         $this->addSql('DROP INDEX IDX_B6BD06EEE7753313 ON quote_responses');
-        $this->addSql('ALTER TABLE quote_responses CHANGE id_van van_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE quote_responses DROP id_van');
     }
 }
