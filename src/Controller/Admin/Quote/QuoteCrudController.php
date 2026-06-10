66b7c5f7f1e673c893fc7adb86badadc7f8f6cd4
LucasBertaud <l.bertaud@hotmail.com>
2026-06-10 11:53:38 +0200

    fix: modify language inside easy admin from english to french

--- src/Controller/Admin/Quote/QuoteCrudController.php
+++ src/Controller/Admin/Quote/QuoteCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin\Quote;
 
 use App\Entity\Quote\Quote;
 use App\Form\Type\Quote\FieldType;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
@@ -16,11 +17,21 @@ class QuoteCrudController extends AbstractCrudController
         return Quote::class;
     }
 
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Devis')
+            ->setEntityLabelInPlural('Devis')
+            ->setSearchFields(['name', 'description']);
+    }
+
     public function configureFields(string $pageName): iterable
     {
         yield TextField::new('name', 'Titre du devis');
 
-        yield TextEditorField::new('description', 'Description du devis');
+        yield TextEditorField::new('description', 'Description du devis')->formatValue(function ($value, $entity) {
+            return nl2br($value);
+        });
 
         yield CollectionField::new('fields', 'Champs')
             ->setEntryType(FieldType::class)
